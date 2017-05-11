95c0f0c4f3f5dbbb4254a599b909704754b2ed80
amalakamala <amalakamala.a@gmail.com>
2017-05-11 13:53:24 -0300

    ahora funciona

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,12 @@
-document.getElementById('toggle').addEventListener("click", function(event){
+document.getElementById('toggle').addEventListener("click",function(event){
 	event.preventDefault();
-	document.getElementById("nav-header").classList.toggle("open");
+	document.getElementById("header").classList.toggle("open");
 	document.getElementById("body").classList.toggle("overflow-hidden");
 });
 
 var lastScrollTop =0;
-
 window.addEventListener("scroll", function(){
-	var currentScroll = window.pageYoffset || document.body.scrollTop;
+	var currentScroll = window.pageYOffset || document.body.scrollTop;
 	if(currentScroll > lastScrollTop){
 		document.getElementById("nav-header").classList.remove("header-in");
 		document.getElementById("nav-header").style.opacity = 0;
